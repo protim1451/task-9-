f548821d6ad1b3bf02b96aa7bb1d20f86a40f075
protim1451 <partha.doict@gmail.com>
2024-04-12 20:28:08 +0600

    animate.css added

--- src/pages/PropertyDetails/PropertyDetails.jsx
+++ src/pages/PropertyDetails/PropertyDetails.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { FaLocationDot } from "react-icons/fa6";
+import 'animate.css';
 
 const PropertyDetails = () => {
     const { id } = useParams();
@@ -26,18 +28,25 @@ const PropertyDetails = () => {
         };
 
         fetchPropertyDetails();
-    }, [id]); // Adding [id] as a dependency
-
+    }, [id]);
     return (
         <div>
             {property ? (
-                <div className="hero bg-base-200">
+                <div className="hero bg-base-200 animate__animated animate__fadeInUp">
                     <div className="hero-content flex-col lg:flex-row">
                         <img src={property.image} className="rounded-lg shadow-2xl lg:w-1/2" alt={property.estate_title} />
                         <div className="lg:w-1/2">
                             <h1 className="text-5xl font-bold">{property.estate_title}</h1>
-                            <p className="py-6">{property.description}</p>
-                            <button className="btn btn-primary">Get Started</button>
+                            <p className="py-3">{property.description}</p>
+                            <div className="py-2">
+                                <h2 className="text-xl font-bold">Facilities:</h2>
+                                <ul className="list-disc ml-6">
+                                    {property.facilities.map((facility, index) => (
+                                        <li key={index}>{facility}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <p className="badge badge-outline"><FaLocationDot />{' '}{property.location}</p>
                         </div>
                     </div>
                 </div>
